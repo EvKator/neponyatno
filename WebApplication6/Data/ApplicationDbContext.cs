4607d0b5ef5457065687a9cd9f506b44a68b0189
islyam <islyambagirov@gmail.com>
2019-12-04 02:11:52 +0200

    initial commit

---
+++ WebApplication6/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication6.Data.Entity;
+
+namespace WebApplication6.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int >
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebApplication6/Data/Entity/ApplicationRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication6.Data.Entity
+{
+    public class ApplicationRole: IdentityRole<int>
+    {
+    }
+}
